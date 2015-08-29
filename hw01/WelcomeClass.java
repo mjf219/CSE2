887460bcd88d21d40b609f1efad017d54ce6a254
mjf219 <mjf219@lehigh.edu>
2015-08-29 19:54:46 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+  
+public class WelcomeClass{
+    
+    public static void main(String[ ] args) {
+  
+  
+System.out.println("-----------");
+System.out.println("| WELCOME |");  
+System.out.println("-----------");  
+System.out.println("^  ^  ^  ^  ^  ^");  
+System.out.println(" / \/ \/ \/ \/ \/ \ "); 
+System.out.println("|-m--j--f--2--1--9-|");
+System.out.println(" \ /\ /\ /\ /\ /\ / ");
+System.out.println("v  v  v  v  v  v"); 
+System.out.println("I am from the US but have lived overseas for most of my life!");
+    
+    }
+    
+}
+  
