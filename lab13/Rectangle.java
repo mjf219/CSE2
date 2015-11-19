2d55220b00ab2c15df128c5e0d6d65eb561fd29d
mjf219 <mjf219@lehigh.edu>
2015-11-19 17:03:42 +0000

    updated lab13

---
+++ lab13/Rectangle.java
@@ -0,0 +1,34 @@
+public class Rectangle
+{
+    double width;
+    double height;
+    
+    public void setWidth(double x)
+    {
+        width = x;
+    }
+    
+    public void setHeight(double y)
+    {
+        height = y;
+    }
+    
+    public double getArea ()
+    {
+        return width*height;
+    }
+    
+    
+    
+    
+    public static void main(String [] args)
+    {
+        
+        Rectangle r1 = new Rectangle();
+        r1.setHeight(5.3);
+        r1.setWidth(7.3);
+        System.out.println(r1.getArea());
+        
+    }
+    
+}
